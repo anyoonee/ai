6cd13a21dafec1a71ea858e7c3597519bad36487
anyoonee <an8399@naver.com>
2023-12-13 14:19:56 +0900

    다형성

--- javaBasic4/src/javaBasic4/ch07/sec06/SnowTireExample.java
+++ javaBasic4/src/javaBasic4/ch07/sec06/SnowTireExample.java
@@ -5,6 +5,13 @@ public class SnowTireExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		SnowTire snowTire = new SnowTire();
+		Tire tire = snowTire;
+		
+		snowTire.run();
+		tire.run();
+		
+		
 	}
 
 }
---
+++ javaBasic4/src/javaBasic4/ch07/sec13/DB.java
@@ -0,0 +1,12 @@
+package javaBasic4.ch07.sec13;
+
+public class DB {
+
+	public void run() {
+		System.out.println("데이터를 저장합니다.");
+		
+		
+
+	}
+	
+}
